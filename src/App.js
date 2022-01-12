75426333197c552c9296a2c51eac43ae680bbc3f
marchel <marioniko001@gmail.com>
2022-01-12 16:13:27 +0200

    fixed typos

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 //Pages and components
-import Apply from './pages/Apply'
-import Contacts from './pages/Contacts'
-import Home from './pages/Home'
-import Pricing from './pages/Pricing'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Apply from "./pages/Apply";
+import Contacts from "./pages/Contacts";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/apply" element={<Apply/>} />
-        <Route path="/contacts" element={<Contacts/>} />
-        <Route path="/pricing" element={<Pricing/>} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apply" element={<Apply />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </BrowserRouter>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Home() {
     return (
-        <div>
-            
+        <div className='flex bg-indigo-900'>
+            test534435435435
         </div>
     )
 }
