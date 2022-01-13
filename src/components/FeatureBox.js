c8c480812ec343a5d1d1e22bb8a12e568bdd3032
marchel <marioniko001@gmail.com>
2022-01-13 23:09:20 +0200

    remove capitalization in featuresbox

--- src/components/FeatureBox.js
+++ src/components/FeatureBox.js
@@ -6,7 +6,7 @@ export default function FeatureBox(props) {
               <span class="inline-block text-blue-500 dark:text-blue-400">
                 <img src={props.image} className='w-12 h-12'/>
               </span>
-              <h1 class="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+              <h1 class="text-2xl font-semibold text-gray-700 dark:text-white">
                 {props.title}
               </h1>
               <p class="text-gray-500 dark:text-gray-300">
