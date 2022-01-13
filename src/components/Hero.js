c742c3980e77300dfacfa3508cf35ba6914ddb66
marchel <marioniko001@gmail.com>
2022-01-13 17:02:17 +0200

    add two components

---
+++ src/components/Hero.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import heroimg from "../assets/images/heroimg.png";
+
+export default function Hero() {
+    return (
+      <div className="relative">
+        <div className="container flex flex-col-reverse mb-3 xl:flex-row items-center gap-6 mt-6">
+          <div className="flex flex-1 flex-col items-center">
+            <h2 className="text-gray-800 text-center text-2xl md:text-3 xl:text-4xl mb-6">
+              Запознайте се с нас!
+            </h2>
+            <p className="text-gray-400 lg:text-lg sm:text-sm text-center mb-6 ">
+              IT Services предлага професионална, дистанционна поддръжка която
+              включва: почистване от вируси, решаване на софтуерни проблеми,
+              инсталация на драйвери, инсталация на софтуер, софтуерна
+              диагностика, и помощ при разрешаване на вашите проблеми
+            </p>
+            <div className="flex justify-center flex-wrap gap-6 mt-3">
+              <button class="bg-indigo-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">
+                Пусни своята заявка
+              </button>
+              <button class="bg-gray-300 shadow-2xl hover:bg-indigo-500 text-black font-bold py-2 px-4 rounded-md">
+                Имате въпроси?
+              </button>
+            </div>
+          </div>
+          <div className="flex justify-center flex-1 mb-10 md:mb-16 lg:mb-0">
+            <img src={heroimg} className="w-4/6 h-4/6" />
+          </div>
+        </div>
+      </div>
+    )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ export default function Navbar() {
                   </div>
 
                   <div className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">
-                  <Link to="/pricing">Услуги</Link>
+                  <Link to="/pricing">Цени</Link>
                   </div>
 
                   <div className="text-white px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700">
@@ -36,47 +36,16 @@ export default function Navbar() {
               </div>
             </div>
             <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={()=> setOpen(!open)}
-                type="button"
-                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
+              <button onClick={()=> setOpen(!open)} type="button" className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
                 {!open ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
+                  <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
                   </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
+                  ) : ( <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
                   </svg>
-                )}
+                  )}
               </button>
             </div>
           </div>
