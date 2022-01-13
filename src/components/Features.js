e961877d1bab1e343e25e00f9c134ece6cf185e7
marchel <marioniko001@gmail.com>
2022-01-14 00:13:40 +0200

    add mobile responsiveness to features component

--- src/components/Features.js
+++ src/components/Features.js
@@ -11,10 +11,10 @@ export default function Features() {
     return (
         <div class="bg-white dark:bg-gray-900 mt-32">
         <div class="container px-6 py-10 mx-auto">
-          <h1 class="w-full text-3xl font-semibold text-gray-800 lg:text-4xl dark:text-white flex justify-center">
+          <h1 class="w-full text-2xl lg:text-4xl text-center font-semibold text-gray-800  dark:text-white flex justify-center">
             Какви услуги предлагаме?
           </h1>
-          <p class="text-lg mt-4 text-gray-500 xl:mt-6 dark:text-gray-300 justify-center flex">
+          <p class="text-md mt-4 text-gray-500 xl:mt-6 dark:text-gray-300 justify-center flex lg:text-lg text-center">
             Нашите специалисти ще Ви предложат професионална, дистанционна поддръжка която включва
           </p>
           <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-2 text-center">
@@ -23,15 +23,15 @@ export default function Features() {
            <FeatureBox image={software} title='Софтуерна диагностика' content='Софтуерни проблеми може да възникнат от некоректно работещи драйвери на хардуерни устройства, от нежелани програми и вируси, както и от остарели версии на програми, несъвместими с останалите софтуерни компоненти в една операционна система.'/>
            <FeatureBox image={printer} title='Настройка на периферно устройство' content='Настройката на периферни устройства е услуга, която включва изтегляне на последна версия на драйвер за конкретното устройство (принтер, скенер, копир и др.), проверка на свързаността и инсталиране на драйвъра.'/>
           </div>
-          <p class="text-lg mt-4 text-gray-500 xl:mt-6 dark:text-gray-300 justify-center flex">
+          <p class="text-md mt-4 text-gray-500 xl:mt-6 dark:text-gray-300 justify-center flex lg:text-lg text-center">
             *Като единственото изискване за онлайн услуги е да имате една от следните програми инсталирани*
           </p>
           <div className='flex mt-3 justify-center'>
               <a href="https://www.teamviewer.com/en/" target="_blank">
-                <img src={teamviewer} className='w-16 h-16 mr-8'/>
+                <img src={teamviewer} className='w-12 h-12 lg:w-16 lg:h-16 mr-8'/>
                 </a>
               <a href="https://www.ultraviewer.net/en/download.html" target="_blank">
-                <img src={ultraviewer} className='w-16 h-16'/>
+                <img src={ultraviewer} className='w-12 h-12 lg:w-16 lg:h-16'/>
                 </a>
             </div>
         </div>
