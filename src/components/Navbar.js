02c5b4f32719962a5d89b765ced7d7869b27fa1e
marchel <marioniko001@gmail.com>
2022-01-13 13:13:47 +0200

    add responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,111 @@
 import { React, useState } from "react";
+import {Transition} from '@headlessui/react'
 import { Link } from "react-router-dom";
-import hamburger from "../assets/images/hamburger.png";
 
 export default function Navbar() {
   let [open, setOpen] = useState(false);
-
   return (
-    <div className="w-full top-0 left-0">
-      <div className="md:flex items-center justify-between bg-white py-4 md:px-10 px-7">
-        <div className="font-bold text-2xl cursor-pointer flex items-center text-gray-800">
-          <span className="text-3x1 text-indigo-600 mr-2 pt-2">
-            <ion-icon name="radio-outline"></ion-icon>
-          </span>
-          <Link to="/">IT Services</Link>
-          <div onClick={() => setOpen(!open)} className="absolute right-8 top-6 cursor-pointer md:hidden">
-            <img src={hamburger} name={open ? "close" : "menu"} alt="hamburger"/>
+    <div>
+      <nav className="bg-indigo-600">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center h-16">
+            <div className="flex items-center w-full justify-between">
+              <div className='flex items-center'>
+                <span className="text-3xl text-white mr-2 pt-2">
+                <ion-icon name="radio-outline"></ion-icon>
+                </span>
+                  <Link to="/" className="text-xl text-white">IT Services</Link>
+                </div>
+               <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <div className="text-white px-3 py-2 rounded-md text-sm font-medium">
+                  <Link to="/contacts">Контакти</Link>
+                  </div>
+
+                  <div className="text-white px-3 py-2 rounded-md text-sm font-medium">
+                  <Link to="/pricing">Услуги</Link>
+                  </div>
+
+                  <div className="text-white px-3 py-2 rounded-md text-sm font-medium">
+                  <Link to="/apply">Работи от вкъщи</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={()=> setOpen(!open)}
+                type="button"
+                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!open ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
           </div>
         </div>
-        <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] w-full left-0 md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? "top-20" : "top-[-490px]"}`}>
-          <li className="mr-6 md:my-0 my-7 text-gray-500 hover:text-indigo-600">
-            <Link to="/contacts">Контакти</Link>
-          </li>
-          <li className="mr-6 md:my-0 my-7 text-gray-500 hover:text-indigo-600">
-            <Link to="/pricing">Услуги</Link>
-          </li>
-          <li className="mr-6 md:my-0 my-7 text-gray-500 hover:text-indigo-600">
-            <Link to="/apply">Работи от вкъщи</Link>
-          </li>
-        </ul>
-      </div>
+        <Transition
+          show={open}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                  <div className="text-white  px-3 py-2 rounded-md text-sm font-medium">
+                  <Link to="/contacts">Контакти</Link>
+                  </div>
+
+                  <div className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  <Link to="/pricing">Услуги</Link>
+                  </div>
+
+                  <div className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  <Link to="/apply">Работи от вкъщи</Link>
+                  </div>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
     </div>
   );
 }
