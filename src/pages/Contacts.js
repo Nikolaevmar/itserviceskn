9c5619235b7622874ba4a84918d0e05437f66b29
marchel <marioniko001@gmail.com>
2022-01-20 23:44:28 +0200

    rename duplicate component

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import ContactsContainer from "../components/ContactsContainer";
 
 export default function Contacts() {
-    return (
-        <div>
-            hell
-        </div>
-    )
+  return (
+    <div className="bg-background bg-no-repeat">
+      <ContactsContainer />
+    </div>
+  );
 }
